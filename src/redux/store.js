c2453fb1a6480858a711a1066390ee8ab1627b2c
Dinara83 <mavleeva.dinara@gmail.com>
2023-02-23 21:56:24 +0200

    redux, react-redux,devtools

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
+
+const appState = {
+  contacts: [],
+  filter: '',
+};
+
+const rootReducer = (state = appState, action) => {
+  return state;
+};
+const enhancer = devToolsEnhancer();
+export const store = createStore(rootReducer);
